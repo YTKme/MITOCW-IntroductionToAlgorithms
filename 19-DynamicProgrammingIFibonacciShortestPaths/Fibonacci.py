1408505cdaf0497aa1863ccd118f3f8fea4c535b
Yan Kuang <YTKme@Outlook.com>
2019-05-20 07:05:58 -0700

    Add first fibonacci function

--- 19-DynamicProgrammingIFibonacciShortestPaths/Fibonacci.py
+++ 19-DynamicProgrammingIFibonacciShortestPaths/Fibonacci.py
@@ -1,10 +1,34 @@
 """Fibonacci
 """
 
+def _fibonacci(n):
+    """Fibonacci base function (naive recursive algorithm)
+    Exponential Time
+
+    Args:
+        n: The nth number of the fibonacci sequence
+
+    Returns:
+        An integer of the nth number in the fibonacci sequence
+    """
+
+    if n <= 2:
+        # Base case
+        f = 1
+    else:
+        # Recursive call
+        f = _fibonacci(n - 1) + _fibonacci(n - 2)
+    
+    return f
+
+
 def main():
     """Main
     """
-    pass
+    _fibonacci_number = 10
+
+    answer = _fibonacci(_fibonacci_number)
+    print("Fibonacci Number {}: {}".format(_fibonacci_number, answer))
 
 if __name__ == "__main__":
     # Execute only if run as a script
